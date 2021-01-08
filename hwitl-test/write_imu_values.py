ad67d617f6986c231c70654890fec8ccc36239f8
claudio mandrioli <claudio@claudios-MacBook-Pro.local>
2021-01-08 15:55:08 +0100

    write both accelerometer data and gyro data

--- hwitl-test/write_imu_values.py
+++ hwitl-test/write_imu_values.py
@@ -43,29 +43,47 @@ sensor data in accelRaw and gyroRaw are int16 written in C2
 for this reason they are to be read with mdh 
 (memory dump halfword - halfword is 16bit)
 '''
+'''
+the memory addresses for the accelRaw and gyroRaw variables
+are found with a breakpoint in the sensorsTask function 
+-- i.e. in gdb insert the command 'b sensors_bmi088_bmp388.c:289' --
+and then ask for 'p &accelRaw'. Otherwise the variable 
+is probably being masked by some other definition that is not being used
+'''
 
 value = 0
 
 while (1) :
 
 	#write accelRaw.x data
-	value = (1*value)
-	stringa = format("mww 0x2000c810 %d \n" % value)
+	stringa = format("mwh 0x20008fdc %d \n" % value)
 	tn.write(stringa.encode())
-	tmp = tn.read_until(b"\r\n") 
-	# print(tmp)
+	tn.read_until(b"\r\n") 
 
-	# #write accelRaw.y data
-	# tn.write(b"mwh 0x20008fde \n")
-	# tmp1 = tn.read_until(b"0x20008fde: ") # read interaction with openocd -- not interesting
-	# tmp2 = tn.read_until(b"\r\n") # read actual memory read
-	# print("accelRaw.y = %d " % translationfromC2(int(tmp2.decode('ascii').replace(" ", "").split()[0],16)), end =",")
+	#write accelRaw.y data
+	stringa = format("mwh 0x20008fde %d \n" % value)
+	tn.write(stringa.encode())
+	tn.read_until(b"\r\n") 
 
 	# #write accelRaw.z data
-	# tn.write(b"mwh 0x20008fe0 \n")
-	# tmp1 = tn.read_until(b"0x20008fe0: ") # read interaction with openocd -- not interesting
-	# tmp2 = tn.read_until(b"\r\n") # read actual memory read
-	# print("accelRaw.z = %d " % translationfromC2(int(tmp2.decode('ascii').replace(" ", "").split()[0],16)))
+	stringa = format("mwh 0x20008fe0 %d \n" % value)
+	tn.write(stringa.encode())
+	tn.read_until(b"\r\n") 
+
+	#write gyroRaw.x data
+	stringa = format("mwh 0x2000916c %d \n" % value)
+	tn.write(stringa.encode())
+	tn.read_until(b"\r\n") 
+
+	#write gyroRaw.y data
+	stringa = format("mwh 0x2000916e %d \n" % value)
+	tn.write(stringa.encode())
+	tn.read_until(b"\r\n") 
+
+	# #write gyroRaw.z data
+	stringa = format("mwh 0x20009170 %d \n" % value)
+	tn.write(stringa.encode())
+	tn.read_until(b"\r\n") 
 
 	time.sleep(0.00001)
 
