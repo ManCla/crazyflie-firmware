079ea9bebce038e07864c83248e47851a98a818c
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2021-01-23 10:28:27 +0100

    implemented translation of signals to lest significant bit when writing to imu variables

--- hwitl-test/write_imu_values.py
+++ hwitl-test/write_imu_values.py
@@ -23,6 +23,36 @@ def translationtoC2(num) :
 	else :
 		return pow(2,16)-num
 
+'''
+this function formats an acceleration value to the format of the
+accelRaw_hitl variable of the crazyflie firmware. 
+
+TODO: the accelScale is not yet accounted for (since it is defined on startup)
+TODO : check for overflow after scaling
+'''
+#def formatAccel(num) : 
+#    if num<0 :
+#        num = pow(2,17) - pow(2,15) + num # C2 representation ?????
+#    return int(num*65536/(2*24))
+def formatAccel(num) : 
+    num = num * 65536/(2*24)
+    if num<0 :
+        num = pow(2,17) + num
+    return int(num)
+
+'''
+this function formats an gyro value to the format of the
+gyroRaw_hitl variable of the crazyflie firmware. 
+
+TODO : the gyroBias is not yet accounted for (since it is defined on startup)
+TODO : check for overflow after scaling
+'''
+def formatGyro(num) : 
+    #num=num+0.148
+    num = num * 65536/(2*2000)
+    if num<0 :
+        num = (pow(2,17) + num) # C2 (pow(2,17)-1)
+    return int(num)
 
 ################## END HELPER FUNC ##################
 
@@ -51,41 +81,43 @@ to make sure you are in the right scope:
 b sensors_bmi088_bmp388.c:300
 '''
 
-value = translationtoC2(0)
+#value = pow(2,17)-pow(2,15)-1
+#value = 10
+#value = int(value*65536/(2*24))
 
-while (1) :
+value= 123
 
-	#write accelRaw_hitl.x data
-	stringa = format("mwh 0x20008e7a %d \n" % value)
-	tn.write(stringa.encode())
-	tn.read_until(b"\r\n") 
 
-	#write accelRaw_hitl.y data
-	stringa = format("mwh 0x20008e7c %d \n" % value)
-	tn.write(stringa.encode())
-	tn.read_until(b"\r\n") 
+#write accelRaw_hitl.x data
+stringa = format("mwh 0x20008e7a %d \n" % formatAccel(value) )
+tn.write(stringa.encode())
+tn.read_until(b"\r\n") 
 
-	# #write accelRaw_hitl.z data
-	stringa = format("mwh 0x20008e7e %d \n" % value)
-	tn.write(stringa.encode())
-	tn.read_until(b"\r\n") 
+#write accelRaw_hitl.y data
+stringa = format("mwh 0x20008e7c %d \n" % formatAccel(value))
+tn.write(stringa.encode())
+tn.read_until(b"\r\n") 
 
-	#write gyroRaw_hitl.x data
-	stringa = format("mwh 0x20009012 %d \n" % value)
-	tn.write(stringa.encode())
-	tn.read_until(b"\r\n") 
+# #write accelRaw_hitl.z data
+stringa = format("mwh 0x20008e7e %d \n" % formatAccel(value))
+tn.write(stringa.encode())
+tn.read_until(b"\r\n") 
 
-	#write gyroRaw_hitl.y data
-	stringa = format("mwh 0x20009014 %d \n" % value)
-	tn.write(stringa.encode())
-	tn.read_until(b"\r\n") 
+#write gyroRaw_hitl.x data
+stringa = format("mwh 0x20009012 %d \n" % formatGyro(value))
+print(stringa)
+tn.write(stringa.encode())
+tn.read_until(b"\r\n") 
 
-	#write gyroRaw_hitl.z data
-	stringa = format("mwh 0x20009016 %d \n" % value)
-	tn.write(stringa.encode())
-	tn.read_until(b"\r\n") 
+#write gyroRaw_hitl.y data
+stringa = format("mwh 0x20009014 %d \n" % formatGyro(value))
+tn.write(stringa.encode())
+tn.read_until(b"\r\n") 
 
-	time.sleep(0.001)
+#write gyroRaw_hitl.z data
+stringa = format("mwh 0x20009016 %d \n" % formatGyro(value))
+tn.write(stringa.encode())
+tn.read_until(b"\r\n") 
 
 tn.close()
 
